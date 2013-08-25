a47c0bf3f7c4996b2fd37feaea43c6d3936e137a
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-08-25 06:31:44 +0000

    Updated shell script.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5615 1c3fd406-9068-4342-8353-511fcdf43622

--- example/run-example.sh
+++ example/run-example.sh
@@ -5,6 +5,7 @@ LPATH="/home/eecs/rubio/build-llvm-3.0/Release/bin/"
 export CC=$LPATH"clang -use-gold-plugin"
 export RANLIB="/bin/true"
 export LDFLAGS="-lmonitor -L/home/eecs/rubio/corvette-instrumentor/src"
+export LD_LIBRARY_PATH=/home/eecs/rubio/corvette-instrumentor/src:$LD_LIBRARY_PATH # may need to be done by hand
 
 $CC -c -emit-llvm -g $1.c -o $1.bc
 
