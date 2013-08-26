77c4e89408e48f4ecee461c312c306322f3bc8f0
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-08-26 00:01:26 +0000

    Created bitcode for some core utilities.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5616 1c3fd406-9068-4342-8353-511fcdf43622

--- example/instrument-example.sh
+++ example/instrument-example.sh
@@ -7,10 +7,10 @@ export RANLIB="/bin/true"
 export LDFLAGS="-lmonitor -L/home/eecs/rubio/corvette-instrumentor/src"
 export LD_LIBRARY_PATH=/home/eecs/rubio/corvette-instrumentor/src:$LD_LIBRARY_PATH # may need to be done by hand
 
-$CC -c -emit-llvm -g $1.c -o $1.bc
+#$CC -c -emit-llvm -g $1.c -o $1.bc
 
 $LPATH/opt -load ../MonitorPass/MonitorPass.so --instrument -f -o tmppass.bc $1.bc
 
 llc tmppass.bc
 
-$CC tmppass.s -o $1.out -L/home/eecs/rubio/corvette-instrumentor/src -lmonitor -lpthread -lm 
+$CC tmppass.s -o $1.out -L/home/eecs/rubio/corvette-instrumentor/src -lmonitor -lpthread -lm -lrt
---
+++ example/instrument-utility.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LPATH="/home/eecs/rubio/build-llvm-3.0/Release/bin/"
+export CC=$LPATH"clang -use-gold-plugin"
+
+# instrument the bitcode file
+$LPATH/opt -load ../MonitorPass/MonitorPass.so --instrument -f -o tmppass.bc $1.bc
+
+# create executable 
+llc tmppass.bc
+$CC tmppass.s -o $1.out -L/home/eecs/rubio/corvette-instrumentor/src -lmonitor -lpthread -lm -lrt
