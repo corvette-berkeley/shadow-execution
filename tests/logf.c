c1e35302cbdfda767fb41e955bff3a0fced44d6e
Cuong Nguyen <nacuong@eecs.berkeley.edu>
2013-09-10 00:09:39 +0000

    Add a simple program logf.c to the regression test. This simple program contains all instructions we instrumented so far
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5656 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/logf.c
+++ tests/logf.c
@@ -3,18 +3,20 @@
 #include <math.h>
 
 int main() {
-  int y = 3;
-  y = y * 2;
-  y = y % 3;
-  y = 4 << y;
-  y = y << 4;
-  float x = 4.0;
-  x = x + 6.0;
-  x = x - 3.0;
-  x = x * 2.0;
-  x = x / 1.2;
-  float result = logf(x);
-  if (x < 3.0)
-    printf("Result %f\n", result);
-  return 0;
+  int x = 5;
+  int y = 4;
+  double z = 1.0;
+  if (x*x-y*y < 2)
+  {
+    x = (int)z*3.0;
+  }
+  else
+  {
+    goto out;
+  }
+
+out:
+  y = x * z;
+
+  return y;
 }
