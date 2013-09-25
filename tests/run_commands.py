3ede067854e5c3871457d81e9cc55bcdeeb0498a
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-09-25 22:55:42 +0000

    Automatically running several instrumented Linux utilities, and checking whether they succeded or not.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5738 1c3fd406-9068-4342-8353-511fcdf43622

---
+++ tests/run_commands.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import proc
+import platform
+import sys
+from subprocess import CalledProcessError, PIPE, Popen, call
+
+
+#
+# main function receives
+#   - argv[1] : list of commands to execute
+#
+def main():
+  executables_name = sys.argv[1]
+  executables = open(executables_name, 'r')
+
+  logfile = "log-run.out"
+  log = open(logfile, "w") 
+
+  ############################################
+  # reading command
+  for line in executables:
+    command = []
+    for token in line.split():
+      if (len(command) == 0):
+        token = "./" + token + ".out" 
+      command.append(token)
+    print command
+
+    # running command
+    retval = call(command, stdin=None, stdout=None, stderr=None)
+
+    # return -1 if running LLVM passes fails
+    if retval <> 0:
+      log.write("[FAILED RUNNING INSTRUMENTED EXECUTABLE]: " + executable + ".out\n")
+
+if __name__ == "__main__":
+  main()
