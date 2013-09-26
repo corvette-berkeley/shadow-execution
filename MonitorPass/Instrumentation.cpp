092539fed1b6a328f784679f0958c8f39c0a7926
Cuong Nguyen <nacuong@eecs.berkeley.edu>
2013-09-26 21:12:21 +0000

    Refactoring of Instrumentation.cpp/h
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5743 1c3fd406-9068-4342-8353-511fcdf43622

--- MonitorPass/Instrumentation.cpp
+++ MonitorPass/Instrumentation.cpp
@@ -1,66 +1,89 @@
+/**
+ * @file Instrumentation.cpp
+ * @brief 
+ */
+
+/*
+ * Copyright (c) 2013, UC Berkeley All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ *
+ * 1.  Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this software must
+ * display the following acknowledgement: This product includes software
+ * developed by the UC Berkeley.
+ *
+ * 4. Neither the name of the UC Berkeley nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY UC BERKELEY ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL UC BERKELEY BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "Common.h"
 #include "Instrumentation.h"
 
-/*******************************************************************************************/
-
-// singleton instance (static)
+/**
+ * Create singleton instrumentation object.
+ */
 Instrumentation* Instrumentation::instance_ = NULL;
 
-/*******************************************************************************************/
+Instrumentation::~Instrumentation() {
+  for(InstrumenterPtrList::iterator itr = instrumenters_.begin();
+      itr < instrumenters_.end();
+      itr = instrumenters_.erase(itr)) {
+    InstrumenterBase* instrumenter = *itr;
+    safe_assert(instrumenter != NULL);
+    delete instrumenter;
+  }
+}
 
 void Instrumentation::RegisterInstrumenter(InstrumenterBase* instrumenter) {
-	instrumenters_.push_back(instrumenter);
+  instrumenters_.push_back(instrumenter);
 }
-
-/*******************************************************************************************/
-
-Instrumentation::~Instrumentation() {
-	for(InstrumenterPtrList::iterator itr = instrumenters_.begin();
-			itr < instrumenters_.end();
-			itr = instrumenters_.erase(itr)) {
-		InstrumenterBase* instrumenter = *itr;
-		safe_assert(instrumenter != NULL);
-		delete instrumenter;
-	}
-}
-
-/*******************************************************************************************/
 
 bool Instrumentation::Initialize(Module &M __attribute__((__unused__))) {
-	return true;
+  return true;
 }
-
-/*******************************************************************************************/
 
 bool Instrumentation::Finalize(Module &M __attribute__((__unused__))) {
-	for(InstrumenterPtrList::iterator itr = instrumenters_.begin();
-			itr < instrumenters_.end();
-			itr = instrumenters_.erase(itr)) {
-		InstrumenterBase* instrumenter = *itr;
-		safe_assert(instrumenter != NULL);
-		outs() << instrumenter->ReportToString();
-	}
-	return true;
+  for(InstrumenterPtrList::iterator itr = instrumenters_.begin();
+      itr < instrumenters_.end();
+      itr = instrumenters_.erase(itr)) {
+    InstrumenterBase* instrumenter = *itr;
+    safe_assert(instrumenter != NULL);
+    outs() << instrumenter->ReportToString();
+  }
+  return true;
 }
-
-/*******************************************************************************************/
 
 void Instrumentation::BeginBasicBlock(BasicBlock* BB, Function* F, Module* M) {
-	safe_assert(BB != NULL);	BB_ = BB;
-	safe_assert(F != NULL);		F_ = F;
-	safe_assert(M != NULL);		M_ = M;
-	AS_ = 0U;
+  safe_assert(BB != NULL);	BB_ = BB;
+  safe_assert(F != NULL);		F_ = F;
+  safe_assert(M != NULL);		M_ = M;
+  AS_ = 0U;
 }
-
-/*******************************************************************************************/
 
 void Instrumentation::BeginFunction() {
   varCount = 0;
   indices.clear();
 }
-
-/*******************************************************************************************/
 
 void Instrumentation::createIndex(uint64_t iid)
 {
@@ -68,36 +91,28 @@ void Instrumentation::createIndex(uint64_t iid)
   varCount++;
 }
 
-/*******************************************************************************************/
-
 int Instrumentation::getIndex(Instruction* inst)
 {
   IID iid = static_cast<IID>(reinterpret_cast<ADDRINT>(inst));
+  safe_assert(indices.find(iid) != indices.end());
   return indices[iid];
 }
-
-/*******************************************************************************************/
 
 int Instrumentation::getFrameSize()
 {
   return varCount;
 }
-/*******************************************************************************************/
 
-// returns true iff any instrumentation was done
 bool Instrumentation::CheckAndInstrument(Instruction* I) {
-	// find an instrumenter to handle the given instruction
-	for(InstrumenterPtrList::iterator itr = instrumenters_.begin(); itr < instrumenters_.end(); ++itr) {
-		InstrumenterBase* instrumenter = *itr;
-		safe_assert(instrumenter != NULL);
-		safe_assert(instrumenter->parent_ == this);
+  // find an instrumenter to handle the given instruction
+  for(InstrumenterPtrList::iterator itr = instrumenters_.begin(); itr < instrumenters_.end(); ++itr) {
+    InstrumenterBase* instrumenter = *itr;
+    safe_assert(instrumenter != NULL);
+    safe_assert(instrumenter->parent_ == this);
 
-		if(instrumenter->CheckAndInstrument(I)) {
-			return true;
-		}
-	}
-	return false;
+    if(instrumenter->CheckAndInstrument(I)) {
+      return true;
+    }
+  }
+  return false;
 }
-
-/*******************************************************************************************/
-
--- MonitorPass/Instrumentation.h
+++ MonitorPass/Instrumentation.h
@@ -1,3 +1,40 @@
+/**
+ * @file Instrumentation.h
+ * @brief Instrumentation Declarations.
+ */
+
+/*
+ * Copyright (c) 2013, UC Berkeley All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ *
+ * 1.  Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this software must
+ * display the following acknowledgement: This product includes software
+ * developed by the UC Berkeley.
+ *
+ * 4. Neither the name of the UC Berkeley nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY UC BERKELEY ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL UC BERKELEY BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef INSTRUMENTATION_H_
 #define INSTRUMENTATION_H_
@@ -12,38 +49,100 @@ class Instrumentation {
 public:
 	typedef std::vector<InstrumenterBase*> InstrumenterPtrList;
 
+  /**
+   * Deconstructor of Instrumentation class.
+   */
 	~Instrumentation();
 
-	bool Initialize(Module &M);
+  /**
+   * Initialize the instrumentation process.
+   *
+   * @param M the LLVM module.
+   *
+   * @return true if the initialization succeeds; false otherwise.
+   */
+  bool Initialize(Module &M);
 
-	bool Finalize(Module &M);
+  /**
+   * Finalize the instrumentation process.
+   *
+   * @param M the LLVM module.
+   *
+   * @return true if the finalization succeeds; false otherwise.
+   */
+  bool Finalize(Module &M);
 
-	void BeginBasicBlock(BasicBlock* BB, Function* F, Module* M);
+  /**
+   * Assign basic block, function and module being instrumented to fields. 
+   *
+   * @param BB the LLVM basic block being instrumented.
+   * @param F the LLVM function being instrumented.
+   * @param M the LLVM module being instrumented.
+   */
+  void BeginBasicBlock(BasicBlock* BB, Function* F, Module* M);
 
+  /**
+   * Reassign the number of variables and their indices to a clean state.
+   *
+   * @note VarCount counts the number of local variables and registers created in
+   * the function. Indices map each variables/registers to a unique index. This
+   * index is used for fast access to the variable during execution.
+   */
   void BeginFunction(); 
 
-	// returns true iff any instrumentation was done
-	bool CheckAndInstrument(Instruction* I);
+  /**
+   * Iterate through all registered instrumenter and instrument the given
+   * instruction.
+   *
+   * @param I the LLVM instruction.
+   *
+   * @return true if at least one instrumenter can be used to instrument the
+   * given instruction; false otherwise
+   */
+  bool CheckAndInstrument(Instruction* I);
 
-  // map an instruction's IID to an index for fast access 
-  // increase the number of variables
+  /**
+   * Create a unique index for variable with id iid.
+   *
+   * @note This function also keep the counter of variables/registers updated.
+   *
+   * @param iid the unique id of a variable
+   */
   void createIndex(uint64_t iid); 
 
+  /**
+   * Get the index of an instruction.
+   *
+   * @param inst the LLVM instruction.
+   *
+   * @return the index of the instruction.
+   */
   int getIndex(Instruction* inst);
 
+  /**
+   * Get the number of variables/registers in the current function.
+   *
+   * @return number of variables/registers in the current function.
+   */
   int getFrameSize();
 
-	inline static Instrumentation* GetInstance() {
-		if(instance_ == NULL) {
-			instance_ = new Instrumentation();
+  inline static Instrumentation* GetInstance() {
+    if(instance_ == NULL) {
+      instance_ = new Instrumentation();
 		}
 		return instance_;
 	}
 
-	void RegisterInstrumenter(InstrumenterBase* instrumenter);
+  /**
+   * Register an instrumenter.
+   *
+   * @note All the instrumenters being registered will be invoked in the order of
+   * registration during instrumentation.
+   */
+  void RegisterInstrumenter(InstrumenterBase* instrumenter);
 
 public:
-	BasicBlock* BB_;
+  BasicBlock* BB_;
 	Function* F_;
 	Module* M_;
 	unsigned AS_;
