78998c2e3717f2d3c2b9b998ef416d160aaf4f44
Cuong Nguyen <nacuong@eecs.berkeley.edu>
2013-10-02 18:52:46 +0000

    Merge BitCastInstrumenter to CastInstructionInstrumenter.;
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5771 1c3fd406-9068-4342-8353-511fcdf43622

--- MonitorPass/BitCastInstrumenter.h
+++ MonitorPass/BitCastInstrumenter.h
@@ -1,15 +1,12 @@
 #ifndef BITCAST_INSTRUMENTER_H_
 #define BITCAST_INSTRUMENTER_H_
 
-#include "Common.h"
-#include "Instrumenter.h"
+#include "CastInstructionInstrumenter.h"
 
 class BitCastInstrumenter : public Instrumenter {
   public:
     BitCastInstrumenter(std::string name, Instrumentation* instrumentation) :
       Instrumenter(name, instrumentation) {};
-
-    virtual bool CheckAndInstrument(Instruction* inst);
 };
 
 #endif
