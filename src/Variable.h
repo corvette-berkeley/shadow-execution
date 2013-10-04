2729f32274db189392145c7c876f041178477cbf
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-10-04 21:44:20 +0000

    Added size and offset data members to class Variable.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5788 1c3fd406-9068-4342-8353-511fcdf43622

--- src/Variable.h
+++ src/Variable.h
@@ -10,14 +10,18 @@ class Variable {
  private:
   KIND type; // see Common.h for definitions
   VALUE value; // union type (use extension later on)
+  unsigned int size;
+  int offset;
   bool local;
   MACHINEFLAG flag;
   void* metadata;
 
  public:
- Variable(KIND t, VALUE v, bool l): type(t), value(v), local(l) {}
+ Variable(KIND t, VALUE v, bool l): type(t), value(v), size(0), offset(0), local(l) {}
 
- Variable(KIND t, bool l): type(t), local(l) {}
+ Variable(KIND t, VALUE v, unsigned int s, int o, bool l): type(t), value(v), size(s), offset(o), local(l) {}
+
+ Variable(KIND t, bool l): type(t), size(0), offset(0), local(l) {}
   
   void setType(KIND t);
 
