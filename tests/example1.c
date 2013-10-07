7af6970ab72e42dafeeaeb7e43ad495cbfa3368e
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-10-07 22:55:56 +0000

    One of the more complicated examples discussed over e-mail.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5809 1c3fd406-9068-4342-8353-511fcdf43622

---
+++ tests/example1.c
@@ -0,0 +1,21 @@
+
+#include <stdlib.h>
+#include <stdio.h> 
+
+int main() {
+  int* a = (int*) malloc(sizeof(int)*10);
+
+  int i = 0;
+  for(; i < 10; i++) {
+    a[i] = i;
+  }
+
+  int* b = a + 2;
+  int c = *b;
+  short* d = (short*)a;
+  short e = *(d+2);
+
+  printf("e: %d", e);
+
+  return 0;
+}
