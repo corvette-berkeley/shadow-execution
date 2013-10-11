820ce25ec223641afa165aa744225f40597b404d
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-10-11 16:35:49 +0000

    Creating text for bitcode as well.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5819 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/instrument-example.sh
+++ tests/instrument-example.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#LPATH="/home/eecs/nacuong/projects/corvette-instrumentor/lib/llvm-3.0.src/build-llvm/Release/bin/"
 LPATH=$GOLD_LLVM_BIN
 
 export CC=$LPATH"/clang -use-gold-plugin"
 export RANLIB="/bin/true"
 export LDFLAGS="-lmonitor -L"$INSTRUMENTOR_PATH"/src"
-#export LD_LIBRARY_PATH=/home/eecs/nacuong/projects/corvette-instrumentor/src:$LD_LIBRARY_PATH # may need to be done by hand
 
 $CC -c -emit-llvm -g $1.c -o $1.bc
+
+llvm-dis $1.bc
 
 $LPATH/opt -load ../MonitorPass/MonitorPass.so --instrument -f -o tmppass.bc $1.bc
 
