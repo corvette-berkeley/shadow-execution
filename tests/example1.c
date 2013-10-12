00cbd19583648b24b82cbec5654efd73bc28a5b0
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-10-12 23:51:06 +0000

    Updated regression test.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5830 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/example1.c
+++ tests/example1.c
@@ -9,13 +9,19 @@ int main() {
   for(; i < 10; i++) {
     a[i] = i;
   }
+  int result = a[3];
 
+  
   int* b = a + 2;
   int c = *b;
+  
+  
   short* d = (short*)a;
+  
   short e = *(d+2);
+  short f = *(d+3);
 
-  printf("e: %d", e);
-
+  printf("e: %d, f: %d", e, f);
+  
   return 0;
 }
