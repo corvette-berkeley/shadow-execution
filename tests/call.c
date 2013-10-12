09d748b53ec38058ac486b1313a427de784f6a05
Cuong Nguyen <nacuong@eecs.berkeley.edu>
2013-10-12 23:31:56 +0000

    Added regression test for passing pointer to function.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5827 1c3fd406-9068-4342-8353-511fcdf43622

---
+++ tests/call.c
@@ -0,0 +1,18 @@
+double addfun(double* a, double* b) 
+{
+  *a = 10;
+  return *a + *b;
+}
+
+int main()
+{
+  double a = 4;
+  double b = 5;
+  double add, sub, mul, div, rem;
+  add = addfun(&a, &b);
+  sub = b - add;
+  mul = add * a;
+  div = mul / sub;
+
+  return div;
+}
