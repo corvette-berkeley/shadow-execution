93bc286420ab6a4f19e9f895169f639de24c20f2
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-10-15 06:20:52 +0000

    Updated regression tests that passes when offset is in the middle of an object.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5849 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/middle-offset.c
+++ tests/middle-offset.c
@@ -10,12 +10,19 @@ int main() {
     a[i] = i;
   }
 
+  int temp = a[3];
+
   short *b = (short*)a;
   short *c = b + 3;
   long *d = (long*)c;
   long result = *(d+1);
 
-  printf("e: %ld", result);
+  printf("e: %ld\n", result);
+
+  *(d+1) = 555;
+
+  a[3] += 1;
+  a[4] += 1;
 
   return 0;
 }
