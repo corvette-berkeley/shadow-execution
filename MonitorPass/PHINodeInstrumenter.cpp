0aa970d858dd507ace4ee7eee8aad25e97c9b7a3
Cuong Nguyen <nacuong@eecs.berkeley.edu>
2013-10-26 19:03:27 +0000

    Added PHINodeInstrumenter.cpp.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5968 1c3fd406-9068-4342-8353-511fcdf43622

---
+++ MonitorPass/PHINodeInstrumenter.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file PHINodeInstrumenter.cpp
+ * @brief
+ */
+
+#include "PHINodeInstrumenter.h"
+
+bool PHINodeInstrumenter::CheckAndInstrument(Instruction* inst) {
+  PHINode* phiNode = dyn_cast<PHINode>(inst);
+
+  if (phiNode != NULL) {
+
+    safe_assert(parent_ != NULL);
+
+    count_++;
+
+    InstrPtrVector instrs;
+
+    Constant* iidC = IID_CONSTANT(phiNode);
+
+    Constant* inxC = computeIndex(phiNode);
+
+    unsigned valuePairs = phiNode->getNumIncomingValues();
+
+    for (unsigned i = 0; i < valuePairs; i++) {
+      Value* value = KVALUE_VALUE(phiNode->getIncomingValue(i), instrs, SIGNED);
+      Constant* blockInxC = computeIndex(phiNode->getIncomingBlock(i)); 
+
+      Instruction* call = CALL_KVALUE_INT("llvm_push_phinode_value", value, blockInxC);
+      instrs.push_back(call);
+    }
+
+    Instruction* call = CALL_IID_INT("llvm_phinode", iidC, inxC);
+    instrs.push_back(call);
+
+    InsertAllAfter(instrs, phiNode);
+
+    return true;
+    
+  } else {
+    
+    return false;
+
+  }
+}
