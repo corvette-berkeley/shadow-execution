1b441e449f6d7d75bc7a2d0eb0011713a6dae6bf
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-10-31 04:37:26 +0000

    New regression test for global variable.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@5996 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/example9.c
+++ tests/example9.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 double a = 8;
 double b;
+double c[5];
+//double* d;
 
 int main() {
+
+  int i;
+  for(i = 0; i < 5; i++) {
+    c[i] = i;
+  }
 
   b = a;
   b += 5.5;
