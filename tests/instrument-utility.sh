720430d7146c5c03fa13a159623efd26d1fab46a
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-11-01 04:20:44 +0000

    Making the script more general.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6001 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/instrument-utility.sh
+++ tests/instrument-utility.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-LPATH="/home/eecs/nacuong/projects/corvette-instrumentor/lib/llvm-3.0.src/bin/bin/"
-# LPATH="/home/eecs/rubio/build-llvm-3.0/Release/bin/"
-export CC=$LPATH"clang -use-gold-plugin"
+LPATH=$GOLD_LLVM_BIN
+export CC=$LPATH"/clang -use-gold-plugin"
+export LDFLAGS="-lmonitor -L"$INSTRUMENTOR_PATH"/src"
 
 # instrument the bitcode file
 $LPATH/opt -load ../MonitorPass/MonitorPass.so --instrument -f -o tmppass.bc $1.bc
 
 # create executable 
 llc tmppass.bc
-$CC tmppass.s -o $1.out -L/home/eecs/nacuong/projects/corvette-instrumentor/src -lmonitor -lpthread -lm -lrt
+$CC tmppass.s -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt
