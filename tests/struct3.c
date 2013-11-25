bfaf864729ec55adfc18eb72bf9a2befaa4f4f31
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2013-11-25 02:05:40 +0000

    Updating list of regression tests.
    Adding new test that reproduces error related to KVALUE_VALUE unsupported.
    
    
    
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6073 1c3fd406-9068-4342-8353-511fcdf43622

---
+++ tests/struct3.c
@@ -0,0 +1,22 @@
+struct point {
+  long a;
+  long b;
+};
+
+//point p;
+
+static struct point foo() {
+  struct point pp;
+  pp.a = 3;
+  pp.b = 4;
+  return pp;
+}
+
+int main() {
+  struct point p;
+  p.a = 1;
+  p.b = 2;
+  p = foo(p);
+  int sum = p.a + p.b;
+  return 0;
+}
