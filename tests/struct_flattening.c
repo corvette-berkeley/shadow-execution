8c69b1f47d78de6e66aefe0e0c241f7b41e81984
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-01-21 00:16:17 +0000

    New regression test reproducing bug encountered in rm util.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6219 1c3fd406-9068-4342-8353-511fcdf43622

---
+++ tests/struct_flattening.c
@@ -0,0 +1,36 @@
+
+#include <stdlib.h>
+#include <stdio.h> 
+
+
+
+struct Y {
+  int x;
+  double y;
+  int z;
+};
+
+struct X {
+  int a;
+  struct Y y;
+  int d;
+  double b;
+  char name[1];
+  char* ptrname;
+};
+
+
+int main() {
+  
+  struct X* x = (struct X*)malloc(sizeof(struct X));
+  x->a = 1;
+  x->y.x = 2;
+  x->y.y = 3.3;
+  x->y.z = 4;
+  x->d = 5;
+  x->b = 2.1;
+  x->ptrname = x->name; // FAIL
+  double c = x->b;
+
+  return 0;
+}
