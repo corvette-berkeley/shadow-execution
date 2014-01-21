fe2c56f64bb5a3afc75b5c010da4d293c94796c3
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-01-21 18:37:36 +0000

    Updating regression test.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6237 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/struct_i64.c
+++ tests/struct_i64.c
@@ -9,17 +9,22 @@ struct mystruct {
 
 
 static struct mystruct foo(struct mystruct *instance) {
-
+  instance->x = 5;
+  instance->y = 6;
+  long a = instance->x + instance->y;
   return *instance;
 }
 
 int main() {
   
   struct mystruct* mstruct = (struct mystruct*)malloc(sizeof(struct mystruct));
-  mstruct->x = 5;
-  mstruct->y = 7;
+  mstruct->x = 1;
+  mstruct->y = 2;
 
-  struct mystruct di = foo(mstruct);
+  struct mystruct di = foo(mstruct); // problematic
+  printf("address of x: %p\n", &(di));
+  printf("address of x: %p\n", &(di.x));
+  printf("address of y: %p\n", &(di.y));
 
   return 0;
 }
