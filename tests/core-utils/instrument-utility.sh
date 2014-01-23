2fc987768bc31d784a85db0253b6af49da2f3c83
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-01-23 00:56:18 +0000

    Modified scripts to include new logger.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6281 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/core-utils/instrument-utility.sh
+++ tests/core-utils/instrument-utility.sh
@@ -2,12 +2,12 @@
 
 LPATH=$GOLD_LLVM_BIN
 export CC=$LPATH"/clang -use-gold-plugin"
-export LDFLAGS="-lmonitor -L"$INSTRUMENTOR_PATH"/src"
+export LDFLAGS="-lmonitor -L"$INSTRUMENTOR_PATH"/src -L"$GLOG_PATH"/lib"
 
 # instrument the bitcode file
 $LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --instrument -f -o tmppass.bc $1.bc
 
 # create executable 
 llc tmppass.bc
-$CC tmppass.s -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt
-#$CC tmppass.s -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -L/home/eecs/rubio/build-gmp-5.1.3/lib -lgmp
+$CC tmppass.s -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lglog
+#$CC tmppass.s -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -L/home/eecs/rubio/build-gmp-5.1.3/lib -lgmp -lglog
