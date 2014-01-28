7da1634f928167a1fd224fb837779309fc9973b7
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-01-28 04:46:34 +0000

    Added a script. Updating lists after running all regressions.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6320 1c3fd406-9068-4342-8353-511fcdf43622

---
+++ tests/nas/instrument.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LPATH=$GOLD_LLVM_BIN
+export CC=$LPATH"/clang -use-gold-plugin"
+export LDFLAGS="-lmonitor -L"$INSTRUMENTOR_PATH"/src -L"$GLOG_PATH"/lib"
+
+# instrument the bitcode file
+$LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --instrument -f -o tmppass.bc $1.bc
+
+# create executable 
+llc tmppass.bc
+$CC tmppass.s -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lgmp -lglog
+
+# create executable for uninstrumented bitcode
+#llc $1.bc
+#$CC $1.s -o $1.out2 -L$GLOG_PATH/lib -lpthread -lm -lrt -lglog
