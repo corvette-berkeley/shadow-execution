797d4100c261ea46533341512cd36dddef6d2256
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-02-02 20:18:35 +0000

    Fixed script. Adding GLOG_log_dir var.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6355 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/regressions/instrument.sh
+++ tests/regressions/instrument.sh
@@ -10,7 +10,7 @@ $CC -c -fpack-struct -emit-llvm -g $1.c -o $1.bc
 
 llvm-dis $1.bc
 
-$LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --instrument -f --file $1-metadata.txt -o tmppass.bc $1.bc
+$LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --instrument -f --file $GLOG_log_dir/$1-metadata.txt -o tmppass.bc $1.bc
 
 llvm-dis tmppass.bc
 $CC tmppass.bc -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lglog 
