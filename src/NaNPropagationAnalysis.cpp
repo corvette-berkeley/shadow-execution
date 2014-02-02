514a2f620b9410ac73542d83aeca3a64dbe69374
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-02-02 20:19:10 +0000

    We now report program locations at which NaN values are loaded or stored. A couple of cases still missing.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6356 1c3fd406-9068-4342-8353-511fcdf43622

--- src/NaNPropagationAnalysis.cpp
+++ src/NaNPropagationAnalysis.cpp
@@ -1,16 +1,38 @@
+
+#include <math.h>
+#include <glog/logging.h>
+
 #include "NaNPropagationAnalysis.h"
+
 
 void NaNPropagationAnalysis::load(IID iid UNUSED, KIND type, KVALUE* src, int file, int line, int inx) {
   InterpreterObserver::load(iid, type, src, file, line, inx);
-  IValue* loadedValue = executionStack.top()[inx];
-  cout << "[NaNPropagation] " << loadedValue->toString() << endl;
+  IValue* loadValue = executionStack.top()[inx];
+  
+  if (type == FLP32_KIND || type == FLP64_KIND || type == FLP128_KIND) {
+    if (isnan(loadValue->getFlpValue())) {
+      LOG(INFO) << file << ":" << line << ": WARNING Loading a NAN value " << endl;
+    }
+  }
   return;
 }
 
 void NaNPropagationAnalysis::store(IID iid UNUSED, KVALUE* dest, KVALUE* src, int file, int line, int inx) {
   InterpreterObserver::store(iid, dest, src, file, line, inx);
-  IValue* destValue = executionStack.top()[dest->inx];
-  cout << "[NaNPropagation] " << destValue->toString() << endl;    
+
+  // TODO: storing nan constant
+  if (src->inx != -1) {
+    IValue* srcValue = executionStack.top()[src->inx];
+    
+    if (srcValue->getType() == FLP32_KIND || srcValue->getType() == FLP64_KIND || srcValue->getType() == FLP128_KIND) {
+      if (isnan(srcValue->getFlpValue())) {
+	LOG(INFO) << file << ":" << line << ": WARNING Storing a NAN Value " << endl;
+      }
+    }
+  }
+
+  // case overwriting a NaN
+
   return;
 }
 
