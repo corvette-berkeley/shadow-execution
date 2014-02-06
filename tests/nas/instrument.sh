493ea42445dc7588497d90c4f32da9faaa71eec1
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-02-06 04:28:15 +0000

    Updated script for NAS benchmarks.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6364 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/nas/instrument.sh
+++ tests/nas/instrument.sh
@@ -7,8 +7,11 @@ export LDFLAGS="-lmonitor -L"$INSTRUMENTOR_PATH"/src -L"$GLOG_PATH"/lib"
 # instrument the bitcode file
 $LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --instrument --file $GLOG_log_dir/$1-metadata.txt -f -o tmppass.bc $1.bc
 
+# move alloca instructions to top of each function
+$LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --move-allocas -f -o tmppass-allocas.bc tmppass.bc
+
 # create executable 
-$CC tmppass.bc -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lgmp -lglog
+$CC tmppass-allocas.bc -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lgmp -lglog
 
 # create executable for uninstrumented bitcode
 #$CC $1.bc -o $1.out2 -L$GLOG_PATH/lib -lpthread -lm -lrt -lglog
