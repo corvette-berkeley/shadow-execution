dcd45e1170f34f4af92d2d2538a8d45bbef2beb6
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-02-25 20:58:04 +0000

    Made script more general to include profiler.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6394 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/regressions/instrument.sh
+++ tests/regressions/instrument.sh
@@ -4,9 +4,14 @@ LPATH=$GOLD_LLVM_BIN
 
 export CC=$LPATH"/clang -use-gold-plugin"
 export RANLIB="/bin/true"
-export LDFLAGS="-lmonitor -L"$INSTRUMENTOR_PATH"/src -L"$GLOG_PATH"/lib -L/home/eecs/nacuong/tools/build/lib"
+export LDFLAGS="-lmonitor -L"$INSTRUMENTOR_PATH"/src -L"$GLOG_PATH"/lib -L"$PROFILER_PATH"/lib" 
 
-$CC -c -fpack-struct -emit-llvm -g $1.c -o $1.bc
+# variables related to profiling
+#export PROFILER=""
+export PROFILER="-lprofiler"
+export CPUPROFILE="a.prof"
+
+$CC -c -fpack-struct -emit-llvm -g -pg $1.c -o $1.bc $PROFILER
 
 llvm-dis $1.bc
 
@@ -16,5 +21,7 @@ $LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --move-allocas -f
 
 llvm-dis tmppass-allocas.bc -o $1-inst.ll
 
-$CC tmppass-allocas.bc -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lglog -lprofiler
+$CC tmppass-allocas.bc -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lglog $PROFILER
 
+# to inspect profiling information
+# pprof --text file.out a.prof
