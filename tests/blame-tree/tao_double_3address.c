3cc9c0f637fbb107b04df0b1906edbeb87b3ba96
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-04-09 21:44:02 +0000

    Double precision version of tao program. As expected, the program prints 'hit'.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6512 1c3fd406-9068-4342-8353-511fcdf43622

---
+++ tests/blame-tree/tao_double_3address.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+  double a, b;
+  double tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, tmp8, tmp9, tmp10, tmp11, tmp12;
+
+  a = 1.84089642f;
+
+  // a*a*a*a
+  tmp1 = a*a;               
+  tmp2 = tmp1*a;            
+  tmp3 = tmp2*a;         
+
+  // 4*a*a*a
+  tmp4 = 4 * a;          
+  tmp5 = tmp4 * a;
+  tmp6 = tmp5 * a;      
+
+  // 6*a*a
+  tmp7 = 6 * a;
+  tmp8 = tmp7 * a;
+
+  // 4 *a
+  tmp9 = 4 *a;
+
+  tmp10 = tmp3 - tmp6;
+  tmp11 = tmp10 + tmp8;
+  tmp12 = tmp11 - tmp9;
+
+  b = tmp12 + 1;
+
+  if (b > 0.499999)
+    printf("hit\n");
+  else
+    printf("miss\n");
+
+  printf("result: %lf\n", b);
+  return 0;
+}
