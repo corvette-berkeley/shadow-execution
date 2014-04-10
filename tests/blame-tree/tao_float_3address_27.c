b421f3e0438e6b1bd0a20cebb5038b4dab65e08d
Cuong Nguyen <nacuong@eecs.berkeley.edu>
2014-04-10 03:56:58 +0000

    Update result for tao_float 27.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6528 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/blame-tree/tao_float_3address_27.c
+++ tests/blame-tree/tao_float_3address_27.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
 
 int main() {
-  float a, b;
-  float tmp0, tmp1, tmp3, tmp4, tmp5, tmp8, tmp11, tmp12;
-  double tmp10, tmp7, tmp9, tmp6, tmp2;
+  double a, b;
+  double tmp1, tmp2, tmp3, tmp5, tmp6, tmp7, tmp8, tmp10, tmp11, tmp12;
+  float tmp4, tmp9;
 
-  a = 1.84089642;
+  a = 1.84089642f;
 
   // a*a*a*a
-  tmp0 = a*a;               
-  tmp1 = tmp0*a;            
-  tmp2 = (double) tmp1 * (double) a; // double         
+  tmp1 = a*a;               
+  tmp2 = tmp1*a;            
+  tmp3 = tmp2*a;         
 
   // 4*a*a*a
-  tmp3 = 4 * a;          
-  tmp4 = tmp3 * a;
-  tmp5 = tmp4 * a;      
+  tmp4 = 4 * a;          
+  tmp5 = tmp4 * a;
+  tmp6 = tmp5 * a;      
 
   // 6*a*a
-  tmp6 = (double) 6 * (double) a; // double
-  tmp7 = tmp6 * a;  // double
+  tmp7 = 6 * a;
+  tmp8 = tmp7 * a;
 
   // 4 *a
-  tmp8 = 4 *a;
+  tmp9 = 4 *a;
 
-  tmp9 = tmp2 - tmp5; // double
-  tmp10 = tmp9 + tmp7; // double
-  tmp11 = (double) tmp10 - (double) tmp8;
+  tmp10 = tmp3 - tmp6;
+  tmp11 = tmp10 + tmp8;
+  tmp12 = tmp11 - tmp9;
 
-  tmp12 = tmp11 + 1;
-
-  b = tmp12;
+  b = tmp12 + 1;
 
   if (b > 0.499999)
     printf("hit\n");
