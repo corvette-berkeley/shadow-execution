cd5aa5e7ac6308b90669d026f91063d82986d7db
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-04-15 18:44:59 +0000

    Better selection of constants. Function call seems fine.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6550 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/blame-tree/function-call-double.c
+++ tests/blame-tree/function-call-double.c
@@ -6,10 +6,19 @@ double fadd(double a, double b) {
 }
 
 int main() {
-  double x = 1.3;
-  double y = 1.3;
+  float fx = 1.3;
+  float fy = 1.123456789;
+  double x = fx;
+  double y = fy;
+
+  x = x + 0.0;
+  printf("x: %.10f\n", x);
+
   double result = fadd(x, y);
+  printf("First result: %.10f\n", result);
+
   result = result + x;
-  printf("%.10f\n", result);
+  printf("Second result: %.10f\n", result);
+
   return 0;
 }
--- tests/blame-tree/function-call-float.c
+++ tests/blame-tree/function-call-float.c
@@ -7,9 +7,16 @@ float fadd(float a, float b) {
 
 int main() {
   float x = 1.3;
-  float y = 1.3;
+  float y = 1.123456789;
+
+  x = x + 0.0; // to be able to inspect trace
+  printf("x: %.10f\n", x);
+
   float result = fadd(x, y);
+  printf("First result: %.10f\n", result);
+
   result = result + x;
-  printf("%.10f\n", result);
+  printf("Second result: %.10f\n", result);
+
   return 0;
 }
