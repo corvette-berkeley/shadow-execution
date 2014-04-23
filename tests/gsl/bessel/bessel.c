23d74490989bec96f18aa07c485c082cacde9d71
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-04-23 22:29:40 +0000

    No need to keep the bitcode around. Removed several functions from include file (cov_*). Program takes input file name as argument.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6616 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/gsl/bessel/bessel.c
+++ tests/gsl/bessel/bessel.c
@@ -1,4 +1,4 @@
-#include "../../../../logging/cov_serializer.h"
+#include "../../../../corvette/logging/cov_serializer.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,16 +7,13 @@
 #include <gsl/gsl_sf_bessel.h>
 
 
-int main () {
+int main (int argc, char *argv[]) {
 
-  long double threshold;
-  double x, y;
-  int i;
-  int j;
+  double x, y, result;
 
-  FILE* infile = fopen("final_inputs", "r");
+  FILE* infile = fopen(argv[1], "r");
   if (!infile) {
-    printf("Could not open final_inputs\n");
+    printf("Could not open input file\n");
   }
 
   char* sx = malloc(100);
@@ -25,7 +22,7 @@ int main () {
     fscanf(infile, "%s", sx);
     x = (double) cov_deserialize(sx, 10);
     y = gsl_sf_bessel_J0 (x);
-    double result = y + 0.0;
+    result = y + 0.0;
   }
 
   printf ("J0(%g) = %.18e\n", x, y);
