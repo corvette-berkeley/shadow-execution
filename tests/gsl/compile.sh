1baa8056c8f1d0ebda69a6b41b274f7eb22940aa
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-04-23 23:13:19 +0000

    Script references to GSL correct bitcode version.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6620 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/gsl/compile.sh
+++ tests/gsl/compile.sh
@@ -7,4 +7,4 @@
 
 clang -g -emit-llvm -c $2/$1.c -I$GSL_PATH/include -o $2/temp_$1.bc
 
-llvm-link -o $2/$1.bc $2/temp_$1.bc $CORVETTE_PATH/benchmarks/gsl/libgsl.so.bc $CORVETTE_PATH/benchmarks/gsl/libgslcblas.so.bc
+llvm-link -o $2/$1.bc $2/temp_$1.bc $CORVETTE_PATH/benchmarks/gsl/libgsl-LLVM3.3.so.bc $CORVETTE_PATH/benchmarks/gsl/libgslcblas-LLVM3.3.so.bc
