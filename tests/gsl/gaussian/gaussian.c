e29cf408868c4ced76b167421930419f513edf50
Cuong Nguyen <nacuong@eecs.berkeley.edu>
2014-04-24 04:04:47 +0000

    Set up gaussian for Erebus. Results in sol.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6627 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/gsl/gaussian/gaussian.c
+++ tests/gsl/gaussian/gaussian.c
@@ -1,22 +1,40 @@
+#include "../../../../precimonious/logging/cov_serializer.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <gsl/gsl_cdf.h>
 
-int main () {
+int main (int argc, char** argv) {
+
+  FILE *infile = fopen(argv[1], "r");
+  if (!infile) {
+    printf("Could not open input file.\n");
+  }
+
   double P, Q;
-  double x = 2.0;
-  double y = 0.0;
+  double x, y;
+
+  char *sx = malloc(10);
+  if (!feof(infile)) {
+    fscanf(infile,"%s",sx);
+    x = (float)cov_deserialize(sx,10);
+  }
 
   P = gsl_cdf_ugaussian_P (x);
-  printf ("prob(x < %f) = %f\n", x, P);
+//  printf ("prob(x < %f) = %f\n", x, P);
 
   Q = gsl_cdf_ugaussian_Q (x);
-  printf ("prob(x > %f) = %f\n", x, Q);
+//  printf ("prob(x > %f) = %f\n", x, Q);
 
   x = gsl_cdf_ugaussian_Pinv (P);
-  printf ("Pinv(%f) = %f\n", P, x);
+//  printf ("Pinv(%f) = %f\n", P, x);
 
   y = gsl_cdf_ugaussian_Qinv (Q);
-  printf ("Qinv(%f) = %f\n", Q, x);
+//  printf ("Qinv(%f) = %f\n", Q, x);
+
+  x = x + 0;
+
+  printf("x = %.10f\n", x);
+  printf("y = %.10f\n", y);
 
   return 0;
 }
