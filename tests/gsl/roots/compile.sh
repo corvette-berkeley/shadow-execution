c121988e9116f3785adaf179bf2c3021b4951dd0
Cuong Nguyen <nacuong@eecs.berkeley.edu>
2014-04-24 02:16:31 +0000

    Setup files to run with Erebus.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6622 1c3fd406-9068-4342-8353-511fcdf43622

---
+++ tests/gsl/roots/compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ###################################
+# Shell script to remove output files
+# Use: ./compile.sh driver_name .
+# ####################################
+
+clang -g -emit-llvm -c $2/$1.c -I$GSL_PATH/include -o $2/temp_$1.bc
+clang -emit-llvm -c $2/demo_fn.c -I$GSL_PATH/include -o $2/demo_fn.bc
+
+llvm-link -o $2/$1.bc $2/temp_$1.bc $2/demo_fn.bc $CORVETTE_PATH/benchmarks/gsl/libgsl-LLVM3.3.so.bc $CORVETTE_PATH/benchmarks/gsl/libgslcblas-LLVM3.3.so.bc
--- tests/gsl/roots/roots.c
+++ tests/gsl/roots/roots.c
@@ -1,3 +1,4 @@
+#include "../../../../precimonious/logging/cov_serializer.h"
 
 #include <stdio.h>
 #include <gsl/gsl_errno.h>
@@ -6,15 +7,40 @@
 
 #include "demo_fn.h"
 
-int main () {
+int main (int argc, char *argv[]) {
+  //
+  // reading input
+  //
+  FILE *infile = fopen(argv[1],"r");
+  if (!infile) {
+    printf("Could not open input file\n");
+  }
+
+  char *sa = malloc(10);
+  char *sb = malloc(10);
+  char *sc = malloc(10);
+  double a, b, c;
+
+  if (!feof(infile)) {
+    fscanf(infile, "%s", sa);
+    fscanf(infile, "%s", sb);
+    fscanf(infile, "%s", sc);
+    a = (float)cov_deserialize(sa, 10);
+    b = (float)cov_deserialize(sb, 10);
+    c = (float)cov_deserialize(sc, 10);
+  }
+
+  //
+  // main computation
+  //
   int status;
   int iter = 0, max_iter = 100;
   const gsl_root_fsolver_type *T;
   gsl_root_fsolver *s;
-  double r = 0, r_expected = sqrt (5.0);
+  double r = 0;
   double x_lo = 0.0, x_hi = 5.0;
   gsl_function F;
-  struct quadratic_params params = {1.0, 0.0, -5.0};
+  struct quadratic_params params = {a, b, -c};
 
   F.function = &quadratic;
   F.params = &params;
@@ -23,35 +49,23 @@ int main () {
   s = gsl_root_fsolver_alloc (T);
   gsl_root_fsolver_set (s, &F, x_lo, x_hi);
 
-  printf ("using %s method\n", 
-          gsl_root_fsolver_name (s));
-
-  printf ("%5s [%9s, %9s] %9s %10s %9s\n",
-          "iter", "lower", "upper", "root", 
-          "err", "err(est)");
-
   do
-    {
-      iter++;
-      status = gsl_root_fsolver_iterate (s);
-      r = gsl_root_fsolver_root (s);
-      x_lo = gsl_root_fsolver_x_lower (s);
-      x_hi = gsl_root_fsolver_x_upper (s);
-      status = gsl_root_test_interval (x_lo, x_hi,
-                                       0, 0.001);
-      
-      if (status == GSL_SUCCESS)
-        printf ("Converged:\n");
-
-      printf ("%5d [%.7f, %.7f] %.7f %+.7f %.7f\n",
-              iter, x_lo, x_hi,
-              r, r - r_expected, 
-              x_hi - x_lo);
-      
-    }
+  {
+    iter++;
+    status = gsl_root_fsolver_iterate (s);
+    r = gsl_root_fsolver_root (s);
+    x_lo = gsl_root_fsolver_x_lower (s);
+    x_hi = gsl_root_fsolver_x_upper (s);
+    status = gsl_root_test_interval (x_lo, x_hi,
+        0, 0.001);
+  }
   while (status == GSL_CONTINUE && iter < max_iter);
 
   gsl_root_fsolver_free (s);
+
+  r = r + 0;
+
+  printf("r = %.10f\n", r);
 
   return status;
 }
