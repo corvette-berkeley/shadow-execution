bfb9e12617c9d3696fe1e3ab487be3e1ded65e74
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-06-04 22:35:15 +0000

    A revised shell script that chooses right library names depending on platform.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6767 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/regressions/instrument.sh
+++ tests/regressions/instrument.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-LPATH=$GOLD_LLVM_BIN
-
-export CC=$LPATH"/clang -use-gold-plugin"
+export CC=$GOLD_LLVM_BIN"/clang -use-gold-plugin"
 export RANLIB="/bin/true"
-export LDFLAGS="-lmonitor -L"$INSTRUMENTOR_PATH"/src -L"$GLOG_PATH"/lib -L"$PROFILER_PATH"/lib" 
+export LDFLAGS="-L"$INSTRUMENTOR_PATH"/src -L"$GLOG_PATH"/lib -L"$PROFILER_PATH"/lib" 
 
 # variables related to profiling
 #export PROFILER=""
 export PROFILER="-lprofiler"
-export CPUPROFILE="a.prof"
+
+platform=`uname -s`
+
+if [ "$platform" == "Darwin" ]; then
+    libmonitor="MonitorPass.dylib"
+else
+    libmonitor="MonitorPass.so"
+fi
 
 # compiling code
-$CC -c -fpack-struct -emit-llvm -g -pg $1.c -o $1.bc $PROFILER
+$CC -c -fpack-struct -emit-llvm -g -pg $1.c -o $1.bc
 llvm-dis $1.bc
 
-# remove constant geps
-$LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --break-constgeps -f -o $1-ngep.bc $1.bc
+# removing constant geps
+$GOLD_LLVM_BIN/opt -load $INSTRUMENTOR_PATH/MonitorPass/$libmonitor --break-constgeps -f -o $1-ngep.bc $1.bc
 llvm-dis $1-ngep.bc
 
 # instrumenting bitcode file
-$LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --instrument -f --file $GLOG_log_dir/$1-metadata.txt --includedFunctions $1-include.txt -o tmppass.bc $1-ngep.bc
+$GOLD_LLVM_BIN/opt -load $INSTRUMENTOR_PATH/MonitorPass/$libmonitor --instrument -f --file $GLOG_log_dir/$1-metadata.txt --includedFunctions $1-include.txt -o tmppass.bc $1-ngep.bc
 
 # removing alloca instructions to top of functions
-$LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --move-allocas -f -o tmppass-allocas.bc tmppass.bc
+$GOLD_LLVM_BIN/opt -load $INSTRUMENTOR_PATH/MonitorPass/$libmonitor --move-allocas -f -o tmppass-allocas.bc tmppass.bc
 llvm-dis tmppass-allocas.bc -o $1-inst.ll
 
 # creating executable
-$CC tmppass-allocas.bc -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lglog $PROFILER
+$CC tmppass-allocas.bc -o $1.out $LDFLAGS -lmonitor -lpthread -lm -lglog $PROFILER
 
 # to inspect profiling information
 # pprof --text file.out a.prof
