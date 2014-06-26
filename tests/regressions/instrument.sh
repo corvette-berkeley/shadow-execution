0607e452f8c2742732081ecb02e97324d4bd939f
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-06-26 22:38:56 +0000

    Made naming convention consistent.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6779 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/regressions/instrument.sh
+++ tests/regressions/instrument.sh
@@ -18,21 +18,23 @@ fi
 
 # compiling code
 $CC -c -fpack-struct -emit-llvm -g -pg $1.c -o $1.bc
-llvm-dis $1.bc
+llvm-dis $1.bc -o $1-orig.ll
 
 # removing constant geps
 $GOLD_LLVM_BIN/opt -load $INSTRUMENTOR_PATH/MonitorPass/$libmonitor --break-constgeps -f -o $1-ngep.bc $1.bc
-llvm-dis $1-ngep.bc
 
 # instrumenting bitcode file
 $GOLD_LLVM_BIN/opt -load $INSTRUMENTOR_PATH/MonitorPass/$libmonitor --instrument -f --file $GLOG_log_dir/$1-metadata.txt --includedFunctions $1-include.txt -o tmppass.bc $1-ngep.bc
 
 # removing alloca instructions to top of functions
 $GOLD_LLVM_BIN/opt -load $INSTRUMENTOR_PATH/MonitorPass/$libmonitor --move-allocas -f -o tmppass-allocas.bc tmppass.bc
-llvm-dis tmppass-allocas.bc -o $1-inst.ll
+llvm-dis tmppass-allocas.bc -o $1.ll
 
 # creating executable
-$CC tmppass-allocas.bc -o $1.out $LDFLAGS -lmonitor -lpthread -lm -lglog $PROFILER
+$CC tmppass-allocas.bc -o $1.out $LDFLAGS -lmonitor -lpthread -lm -lglog $PROFILER #foo.o
+
+# creating executable for original program
+$CC $1.bc -o $1-orig.out $LDFLAGS -lmonitor -lpthread -lm -lglog $PROFILER #foo.o
 
 # to inspect profiling information
 # pprof --text file.out a.prof
