9652de9ae5b4656edd7de92949005dfbee025580
Cindy Rubio Gonzalez <rubio@cs.berkeley.edu>
2014-07-14 04:37:08 +0000

    Using tcmalloc by default when compiling NAS programs.
    git-svn-id: https://repo.eecs.berkeley.edu/svn/users/ksen/projects/corvette-instrumentor@6818 1c3fd406-9068-4342-8353-511fcdf43622

--- tests/nas/instrument.sh
+++ tests/nas/instrument.sh
@@ -5,9 +5,9 @@ export CC=$LPATH"/clang -use-gold-plugin"
 export LDFLAGS="-lmonitor -L"$INSTRUMENTOR_PATH"/src -L"$GLOG_PATH"/lib -L"$PROFILER_PATH"/lib"
 
 # variables related to profiling
-#export PROFILER=""
-export PROFILER="-lprofiler"
-#export PROFILER="-ltcmalloc"
+export PROFILER=""
+#export PROFILER="-lprofiler"
+export MALLOC="-ltcmalloc"
 
 llvm-dis $1.bc -o $1-orig.ll
 
@@ -22,11 +22,11 @@ $LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --move-allocas -f
 llvm-dis tmppass-allocas.bc -o $1.ll
 
 # create executable 
-$CC tmppass-allocas.bc -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lgmp -lglog $PROFILER
-#ld tmppass-allocas.bc -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lgmp -lglog $PROFILER
+$CC tmppass-allocas.bc -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lgmp -lglog $PROFILER $MALLOC
+#ld tmppass-allocas.bc -o $1.out -L$LDFLAGS -lmonitor -lpthread -lm -lrt -lgmp -lglog $PROFILER $MALLOC
 
 # create executable for uninstrumented bitcode
-$CC $1.bc -o $1.out2 -L$LDFLAGS -lpthread -lm -lrt -lgmp -lglog $PROFILER
+$CC $1.bc -o $1.out2 -L$LDFLAGS -lpthread -lm -lrt -lgmp -lglog $PROFILER $MALLOC
 
 # inspecting profile information
 # pprof --text executable.out cpu.prof
