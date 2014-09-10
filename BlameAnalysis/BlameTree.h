f2849c9d49c39b9024c1fa6a1951a5e8ac7093e2
Cuong Nguyen <nacuong.vn@gmail.com>
2014-09-10 14:23:11 -0700

    Create a new folder for blame analysis. Moving all blame analysis files from src folder to the new folder. Make the files in the new folder compilable.

--- BlameAnalysis/BlameTree.h
+++ BlameAnalysis/BlameTree.h
@@ -1,72 +1,42 @@
-/**
- * @file BlameTreeFloat.h
- * @brief BlameTreeFloat Declarations.
- */
+// Author: Cuong Nguyen
 
-/*
- * Copyright (c) 2013, UC Berkeley All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
- *
- * 1.  Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the UC Berkeley nor the names of its contributors may
- * be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY UC BERKELEY ''AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
- * EVENT SHALL UC BERKELEY BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- */
+#ifndef BLAME_TREE_H_
+#define BLAME_TREE_H_
 
-// Author: Cuong Nguyen and Cindy Rubio-Gonzalez
-
-#ifndef BLAME_TREE_FLOAT_H_
-#define BLAME_TREE_FLOAT_H_
-
-#include "InterpreterObserver.h"
 #include "BlameTreeShadowObject.h"
 #include "BlameTreeUtilities.h"
 #include "BlameTreeAnalysis.h"
 #include "BlameNodeID.h"
-#include "IValue.h"
+#include "../src/IValue.h"
+#include "../src/InterpreterObserver.h"
 #include <math.h>
 #include <fstream>
 
 using namespace llvm;
 using namespace std;
 
-class BlameTreeFloat : public InterpreterObserver {
+class BlameTree : public InterpreterObserver {
   
   public:
     static int dynamicCounter; // unique counter for instructions executed
     static map< int, vector<BlameTreeShadowObject<HIGHPRECISION> > > trace;
 
-    BlameTreeFloat(std::string name) : InterpreterObserver(name) {}
+    BlameTree(std::string name) : InterpreterObserver(name) {}
 
     virtual void post_call_sin(IID iid, bool nounwind, int pc, KIND type, int inx, SCOPE argScope, int64_t argValueOrIndex);
 
     virtual void post_call_acos(IID iid, bool nounwind, int pc, KIND type, int inx, SCOPE argScope, int64_t argValueOrIndex);
 
+    virtual void post_call_cos(IID iid, bool nounwind, int pc, KIND type, int inx, SCOPE argScope, int64_t argValueOrIndex);
+
     virtual void post_call_sqrt(IID iid, bool nounwind, int pc, KIND type, int inx, SCOPE argScope, int64_t argValueOrIndex);
 
     virtual void post_call_fabs(IID iid, bool nounwind, int pc, KIND type, int inx, SCOPE argScope, int64_t argValueOrIndex);
 
-    virtual void post_call_cos(IID iid, bool nounwind, int pc, KIND type, int inx, SCOPE argScope, int64_t argValueOrIndex);
+    virtual void post_call_log(IID iid, bool nounwind, int pc, KIND type, int inx, SCOPE argScope, int64_t argValueOrIndex);
+
+    virtual void post_call_floor(IID iid, bool nounwind, int pc, KIND type, int inx, SCOPE argScope, int64_t argValueOrIndex);
+
 
     virtual void post_fadd(SCOPE lScope, SCOPE rScope, int64_t lValue,
 			   int64_t rValue, KIND type, int file, int line, int col, int inx);
@@ -86,7 +56,7 @@ class BlameTreeFloat : public InterpreterObserver {
     virtual void post_fpext(int64_t op, SCOPE opScope, KIND opKind, KIND
         kind, int size, int inx);
 
-    virtual void post_create_global_symbol_table();
+    virtual void pre_analysis();
 
     virtual void post_analysis();
 
@@ -135,10 +105,14 @@ class BlameTreeFloat : public InterpreterObserver {
      * @param constOrIndex constant/index of the value.
      * @return the actual value in its lower precision.
      */
-    HIGHPRECISION getActualValue(SCOPE scope, int64_t constOrIndex);
+    LOWPRECISION getActualValue(SCOPE scope, int64_t constOrIndex);
 
     void post_fbinop(SCOPE lScope, SCOPE rScope, int64_t lValue, int64_t
 		     rValue, KIND type, int file, int line, int col,  int inx, BINOP op);
+
+    void post_lib_call(IID iid, bool nounwind, int pc, KIND type, int inx,
+        SCOPE argScope, int64_t argValueOrIndex, string func);
+
 };
 
 #endif
--- BlameAnalysis/BlameTreeUtilities.h
+++ BlameAnalysis/BlameTreeUtilities.h
@@ -1,52 +1,16 @@
-/**
- * @file BlameTreeUtilities.h
- * @brief BlameTreeUtilities Declarations.
- */
-
-/*
- * Copyright (c) 2013, UC Berkeley All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
- *
- * 1.  Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the UC Berkeley nor the names of its contributors may
- * be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY UC BERKELEY ''AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
- * EVENT SHALL UC BERKELEY BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-// Author: Cuong Nguyen and Cindy Rubio-Gonzalez
+// Author: Cuong Nguyen
 
 #ifndef BLAME_TREE_UTILITIES_H
 #define BLAME_TREE_UTILITIES_H
 
-#include "Common.h"
-#include "Constants.h"
+#include "../src/Common.h"
+#include "../src/Constants.h"
 #include <inttypes.h>
 #include <math.h>
 #include <map>
 #include <set>
 #include <string>
 #include <vector>
-
 
 using namespace std;
 
@@ -78,8 +42,6 @@ typedef enum {
   BIN_INTR, UNARY_INTR, CONSTANT_INTR, CALL_INTR, LOAD_INTR, STORE_INTR, TRUNC_INTR, EXT_INTR,
   INTRTYPE_INVALID
 } INTRTYPE;
-
-
 
 class BlameTreeUtilities {
   public:
