e6bbdc1c796530e8586b4876394e49a7969c6675
Cindy Rubio Gonzalez <cindy.rubio@gmail.com>
2014-09-16 14:00:22 -0700

    Removing warnings when compiling Blame Analysis. Fixed instrumentation script to use the same enviroment variables the main shadow execution engine uses.

--- tests-blameanalysis/instrument.sh
+++ tests-blameanalysis/instrument.sh
@@ -2,7 +2,8 @@
 
 LPATH=$GOLD_LLVM_BIN
 export CC=$LPATH"/clang -use-gold-plugin"
-export LDFLAGS="-L"$INSTRUMENTOR_PATH"/Release+Asserts/lib/ -L"$GLOG_PATH"/lib -L"$PROFILER_PATH"/lib"
+export LDFLAGS="-L"$MONITOR_LIB_PATH"/../Release+Asserts/lib -L"$GLOG_LIB_PATH" -L"$PROFILER_PATH"/lib -L"$INSTRUMENTOR_LIB_PATH
+echo $LDFLAGS
 
 name=$(basename $1 .c)
 
@@ -10,16 +11,16 @@ name=$(basename $1 .c)
 $LPATH/clang -emit-llvm -g -pg -c $name.c -o $name.bc
 
 # Memove constant geps
-$LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --break-constgeps -f -o $name-ngep.bc $name.bc 
+$LPATH/opt -load $MONITOR_LIB_PATH/MonitorPass.so --break-constgeps -f -o $name-ngep.bc $name.bc 
 
 # Instrument the bitcode
-$LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --instrument --file $GLOG_log_dir/debug.bin -f -o tmppass.bc $name-ngep.bc
+$LPATH/opt -load $MONITOR_LIB_PATH/MonitorPass.so --instrument --file $GLOG_log_dir/debug.bin -f -o tmppass.bc $name-ngep.bc
 
 # Move alloca instructions to top of each function
-$LPATH/opt -load $INSTRUMENTOR_PATH/MonitorPass/MonitorPass.so --move-allocas -f -o tmppass-allocas.bc tmppass.bc
+$LPATH/opt -load $MONITOR_LIB_PATH/MonitorPass.so --move-allocas -f -o tmppass-allocas.bc tmppass.bc
 
 # Create executable 
-$CC tmppass-allocas.bc -o $name.out $LDFLAGS -lba -lpthread -lm -lrt -lgmp -lglog
+$CC tmppass-allocas.bc -o $name.out $LDFLAGS -lba -lmonitor -lpthread -lm -lrt -lgmp -lglog
 
 # Clean temporary files
 rm $name.bc
