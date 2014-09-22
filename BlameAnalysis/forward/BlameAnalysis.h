b62c3bcfd7a9482b56cd29c719c73ee5bbf59dd8
Cuong Nguyen <nacuong@live.com>
2014-09-21 18:10:18 -0700

    Added function and field comments.

--- BlameAnalysis/forward/BlameAnalysis.h
+++ BlameAnalysis/forward/BlameAnalysis.h
@@ -10,6 +10,8 @@
 class BlameAnalysis : public InterpreterObserver {
 
 private:
+	// Return the file separator character depending on the underlying operating
+	// system.
 	inline char separator() {
 #ifdef _WIN32
 		return '\\';
@@ -18,9 +20,13 @@ private:
 #endif
 	}
 
+	// Read debug information from $GLOG_log_dir/debug.bin and wrap into a
+	// mapping from instruction IID to DebugInfo struct. The file debug.bin is
+	// constructed during instrumentation phase.
+	//
+	// TODO: find a way to make the name debug.bin to be provided as input and
+	// sounds more personal to the application under analysis.
 	unordered_map<IID, DebugInfo> readDebugInfo() {
-		// Read debug information from $GLOG_log_dir/debug.bin and store into
-		// debugInfoMap.
 		std::stringstream debugFileName;
 		debugFileName << getenv("GLOG_log_dir") << separator() << "debug.bin";
 		FILE* debugFile = fopen(debugFileName.str().c_str(), "rb");
@@ -36,6 +42,8 @@ private:
 		return debugInfoMap;
 	}
 
+	// A map from instruction IID to debug information. IID is computed during
+	// instrumentation phase and is the unique id for each LLVM instruction.
 	const unordered_map<IID, DebugInfo> debugInfoMap = readDebugInfo();
 
 public:
