3796fb5baad24948afdb036aa26604b7115e8789
Ben Mehne <bmehne@gmail.com>
2014-09-26 01:01:48 -0700

    Attempting to debug travis failure

--- tests/nas/travis-main.py
+++ tests/nas/travis-main.py
@@ -18,7 +18,7 @@ names = ["Command", "Return Code", "Runtime", "Max memory usage"]
 f = ['Foobar\n', "0\n", "300\n", "10000000\n"]
 for new_val, str_ref_val, name in zip(result, f, names):
 	str_ref_val = str_ref_val[0:-1]
-	if name == names[0] and new_val != str_ref_val:
+	if name == names[0] and str(new_val) != str_ref_val:
 		print("Command ran was different - error!: Expected "+str_ref_val + " but got " + str(new_val))
 		#exit(1)
 
@@ -28,4 +28,4 @@ for new_val, str_ref_val, name in zip(result, f, names):
 	if new_val > ref_val * (1+FUDGE_PERCENT):
 		print("New value for " + name + " is much worse than old value: " + str(new_val) + " > " + str(ref_val) + " * " + str(1+FUDGE_PERCENT))
 		#exit(1)
-print(result[0]+" returned " + str(result[1]) + ", using " + str(result[2]) + " seconds and " + str(result[3]) + " bytes")
+print(str(result[0])+" returned " + str(result[1]) + ", using " + str(result[2]) + " seconds and " + str(result[3]) + " bytes")
