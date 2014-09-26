8f6a71e1035363bb193485b4d1ed82234a7ca81a
Ben Mehne <bmehne@gmail.com>
2014-09-26 00:42:24 -0700

    Accidently committed highly modified file

--- tests/nas/main.py
+++ tests/nas/main.py
@@ -3,16 +3,10 @@
 # fs-copy dir
 
 import jobmonitor
-import subprocess
 import sys
 
-args=['valgrind', '--soname-synonyms=somalloc=*tcmalloc*', './' + sys.argv[1]]
-f = open('last_run_log.txt', 'w')
-result = jobmonitor.RunJob(args,shell = False, stdout=f, stderr=subprocess.STDOUT)
-f.close()
+args=['./' + sys.argv[1]]
 
-f = open('last_run_log.txt', 'r')
-for a in f:
-	print(a[0:-1])
-print(result)
+print jobmonitor.RunJob(args,shell = False)
+
 
