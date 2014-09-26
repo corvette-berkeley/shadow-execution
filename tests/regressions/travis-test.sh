7c2bdb6c9338c063202f583f6bc83021315599f2
Ben Mehne <bmehne@gmail.com>
2014-09-26 14:04:18 -0700

    Adjusted regression suite to ignore line numbers

--- tests/regressions/travis-test.sh
+++ tests/regressions/travis-test.sh
@@ -5,9 +5,10 @@ cd "$THIS_DIR"
 echo $THIS_DIR
 #output is sanitized to remove
 # the file that the debug info is written to
+# the line numbers for safe_assert faults (since they change by version)
 # the address of pointers in the program (which are highly volatile)
 # the value of undefined values (which are highly volatile)
-python2 small-regressions.py tests.txt 2>/dev/null | sed -e 's/debug map \(\/\([^\/]*\)\)*/\2/g' | grep -v -i 'address of' | grep -v -i 'undefined value' > small_regressions_tests.test.out
+python2 small-regressions.py tests.txt 2>/dev/null | sed -e 's/debug map \(\/\([^\/]*\)\)*/\2/g' | sed -e 's/line: \([0-9]*\)/line: \*/g' | grep -v -i 'address of' | grep -v -i 'undefined value' > small_regressions_tests.test.out
 
 if diff small_regressions_tests.ref small_regressions_tests.test.out
 then
