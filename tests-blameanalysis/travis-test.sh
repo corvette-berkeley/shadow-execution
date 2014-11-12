38dd9042cb47e3bf17713505cfbf24c70e138f0e
Ben Mehne <bmehne@gmail.com>
2014-11-12 14:05:56 -0800

    First set of refactoring

--- tests-blameanalysis/travis-test.sh
+++ tests-blameanalysis/travis-test.sh
@@ -2,29 +2,37 @@
 export THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$THIS_DIR"
 
-echo $THIS_DIR
+function test_this_dir() {
+	while read program
+	do
+		echo "Instrumenting $program ..."
+#		./instrument-forward.sh $program > /dev/null 2>/dev/null
+		./instrumentfp2.sh $program > /dev/null 2>/dev/null
+		echo "Running $program ..."
+		python2 ../travis-main.py $program
+		if [ "$?" != "0" ]
+		then
+			exit 1
+		fi
 
-while read program
+#		sed -i 's/IID [0-9]\+/ IID <ommitted>/' "$program"".out.ba"
+#		sed -i 's/IID [0-9]\+/ IID <ommitted>/' "$program"".ref.ba"
+
+		if diff "$program"".out.ba" "$program"".ref.ba"
+		then
+			echo "No output change"
+			rm "$program"".out.ba"
+		else
+			echo "Output changed!"
+			exit 1
+		fi
+	done < travis-tests.txt
+}
+
+for a in `find . -maxdepth 1 -mindepth 1 -type d`
 do
-	echo "Instrumenting $program ..."
-	#./instrument-forward.sh $program > /dev/null 2>/dev/null
-	./instrumentfp2.sh $program > /dev/null 2>/dev/null
-	echo "Running $program ..."
-	python2 travis-main.py $program
-	if [ "$?" != "0" ]
-	then
-		exit 1
-	fi
-
-#	sed -i 's/IID [0-9]\+/ IID <ommitted>/' "$program"".out.ba"
-#	sed -i 's/IID [0-9]\+/ IID <ommitted>/' "$program"".ref.ba"
-
-	if diff "$program"".out.ba" "$program"".ref.ba"
-	then
-		echo "No output change"
-		rm "$program"".out.ba"
-	else
-		echo "Output changed!"
-		exit 1
-	fi
-done < travis-tests.txt
+	echo $a
+	cd $a
+	test_this_dir
+	cd ..
+done
