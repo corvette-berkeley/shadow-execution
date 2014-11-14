0ec652056f297bc73b607d28f5f8d58a42e611c3
Cindy Rubio Gonzalez <cindy.rubio@gmail.com>
2014-11-14 12:47:07 -0800

    Adding -g flag.

--- tests/blameanalysis/gsl/compile.sh
+++ tests/blameanalysis/gsl/compile.sh
@@ -5,6 +5,6 @@
 # Use: ./compile.sh driver_name .
 # ####################################
 
-clang -emit-llvm -c $2/$1.c -I$GSL_PATH/include -o $2/temp_$1.bc
+clang -emit-llvm -c -g $2/$1.c -I$GSL_PATH/include -o $2/temp_$1.bc
 llvm-link -o $2/$1.bc $2/temp_$1.bc $GSL_LIB_PATH/libgsl.so.bc $GSL_LIB_PATH/libgslcblas.so.bc
 
