c575202b511b75f195416465094d7a8add788075
Ben Mehne <bmehne@gmail.com>
2014-11-14 00:06:39 -0800

    Further refactoring of gsl as requested

--- tests/blameanalysis/gsl/instrumentfp2.sh
+++ tests/blameanalysis/gsl/instrumentfp2.sh
@@ -3,11 +3,11 @@
 export CC=$LLVM_BIN_PATH"/clang"
 export LDFLAGS="-L"$INSTRUMENTOR_LIB_PATH" -L"$BLAMEANALYSIS_LIB_PATH" -L"$GLOG_LIB_PATH" -L"$GPERFTOOLS_LIB_PATH" -L"$UNWIND_LIB_PATH" -L"$FPPASS_LIB_PATH
 
-name=$(basename $1 .c)
+name=$1
 
 # Instrument the bitcode
 $LLVM_BIN_PATH/opt -load $FPPASS_LIB_PATH/FPPass.so -fppass -f -o $name-fp.bc $name.bc
-#-include include_$name.txt 
+#-include include_$name.txt
 
 # Create executable for instrumented bitcode file
 $CC $name-fp.bc -o $name.out $LDFLAGS -lba3 -lpthread -lm -lrt -lgmp -lglog  -ltcmalloc
