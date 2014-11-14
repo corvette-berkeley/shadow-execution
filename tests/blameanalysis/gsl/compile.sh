c44c0744be4370a641a6bbb3be5723e4d511c7b5
Cindy Rubio Gonzalez <cindy.rubio@gmail.com>
2014-11-14 11:49:04 -0800

    Script to compile GSL drivers.

---
+++ tests/blameanalysis/gsl/compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ###################################
+# Shell script to remove output files
+# Use: ./compile.sh driver_name .
+# ####################################
+
+
+clang -emit-llvm -c $2/$1.c -I$GSL_PATH/include -o $2/temp_$1.bc
+
+llvm-link -o $2/$1.bc $2/temp_$1.bc $GSL_LIB_PATH/libgsl.so.bc $GSL_LIB_PATH/libgslcblas.so.bc
+
